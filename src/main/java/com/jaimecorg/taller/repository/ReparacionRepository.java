f04fffd5843741aad54c1f6f918966c01f895265
Jaime Corpas <jaime.corpas.785@estudiante.iesoretania.es>
2023-04-12 10:53:09 +0200

    CRUD Vehículo

---
+++ src/main/java/com/jaimecorg/taller/repository/ReparacionRepository.java
@@ -0,0 +1,9 @@
+package com.jaimecorg.taller.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jaimecorg.taller.model.Reparacion;
+
+public interface ReparacionRepository extends JpaRepository<Reparacion, Integer>{
+    
+}
